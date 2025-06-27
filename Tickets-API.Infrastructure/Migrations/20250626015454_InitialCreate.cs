e70971eff5e099fb97936eb4966ad5c4e7cd8f43
gabflobo <gabrielflobo8@gmail.com>
2025-06-26 22:04:33 -0300

    add: migrations

---
+++ Tickets-API.Infrastructure/Migrations/20250626015454_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickets_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    nomeSolicitante = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
---
+++ Tickets-API.Infrastructure/Migrations/20250626041408_RenameColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickets_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nomeSolicitante",
+                table: "Tickets",
+                newName: "NomeSolicitante");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Tickets",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomeSolicitante",
+                table: "Tickets",
+                newName: "nomeSolicitante");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Tickets",
+                newName: "id");
+        }
+    }
+}
