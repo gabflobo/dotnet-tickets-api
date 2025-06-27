74478efb757b48eef547df9bf44bef01de6dd3e5
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2025-06-26 21:59:48 -0300

    feat: adicionando Eum de status de chamado

---
+++ Tickets-API.Domain/Enums/StatusTicket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets_API.Domain.Enums
+{
+    public enum StatusTicket
+    {
+        Aberto = 1,
+        EmAndamento = 2,
+        Resolvido = 3,
+        Cancelado = 4
+    }
+}
