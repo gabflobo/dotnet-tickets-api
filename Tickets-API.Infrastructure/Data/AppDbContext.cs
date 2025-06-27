42050f1ab0f7ccbb26a969f8182e5d8b02b50ba8
gabflobo <gabrielflobo8@gmail.com>
2025-06-26 22:03:31 -0300

    feat: configurando dbcontext

---
+++ Tickets-API.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickets_API.Domain.Entities;
+
+
+namespace Tickets_API.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Tickets-API.Infrastructure/Data/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets_API.Infrastructure.Data
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=localhost;Database=TicketsDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
