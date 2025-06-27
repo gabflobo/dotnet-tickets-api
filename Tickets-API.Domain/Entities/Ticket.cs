e265718d023f9b314454903b7d02894943da91d2
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2025-06-26 21:59:12 -0300

    feat: entidades Ticket e Base adicionadas

---
+++ Tickets-API.Domain/Entities/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickets_API.Domain.Enums;
+
+namespace Tickets_API.Domain.Entities
+{
+    public class Ticket : BaseEntity
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string NomeSolicitante { get; set; }
+        public StatusTicket Status { get; set; } = StatusTicket.Aberto;
+    }
+}
